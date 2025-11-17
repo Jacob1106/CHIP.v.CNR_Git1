625aa8e3dbea9ddadb4dae6302d1fbf2a73bc7f8
Jacob <jacob@martin-email.com>
2025-11-17 13:15:39 -0600

    Peak calls

--- UnifiedPeakCalling/PeakCallingForCTCF.sh
+++ UnifiedPeakCalling/PeakCallingForCTCF.sh
@@ -2,16 +2,16 @@
 
 #H3K27 CNR
 
-# bsub -P baker -J 'H3K27-PeakCalling' -N -n 8 -R "rusage[mem=16GB]" -R "span[hosts=1]" -q priority  \
-# 'macs3 callpeak -t /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/ChIP.v.CnR/H3K27me3/CnR/SJHGG059115_C145-LTC42_Ctrl-H3K27me3-AB4_R1.target.markdup.sorted.bam /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/ChIP.v.CnR/H3K27me3/CnR/SJHGG059115_C145-LTC42_Ctrl-H3K27me3-AB4_R2.target.markdup.sorted.bam \
-# -c /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/ChIP.v.CnR/H3K27me3/CnR/SJHGG059115_C145-LTC42_Ctrl-IGG-AB14_R1.target.markdup.sorted.bam \
-# -f BAM \
-# -n H3KK27_PeakCalls \
-# -B \
-# -p 0.001 \
-# --cutoff-analysis \
-# --trackline \
-# --outdir /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/CHIP.v.CNR_Git1/UnifiedPeakCalling/CNR_H3K27me3_PeakCalls'
+bsub -P baker -J 'H3K27-PeakCalling' -N -n 8 -R "rusage[mem=16GB]" -R "span[hosts=1]" -q priority  \
+'macs3 callpeak -t /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/ChIP.v.CnR/H3K27me3/CnR/SJHGG059115_C145-LTC42_Ctrl-H3K27me3-AB4_R1.target.markdup.sorted.bam /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/ChIP.v.CnR/H3K27me3/CnR/SJHGG059115_C145-LTC42_Ctrl-H3K27me3-AB4_R2.target.markdup.sorted.bam \
+-c /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/ChIP.v.CnR/H3K27me3/CnR/SJHGG059115_C145-LTC42_Ctrl-IGG-AB14_R1.target.markdup.sorted.bam \
+-f BAM \
+-n H3KK27_PeakCalls \
+-B \
+-p 0.001 \
+--cutoff-analysis \
+--trackline \
+--outdir /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/CHIP.v.CNR_Git1/UnifiedPeakCalling/CNR_H3K27me3_PeakCalls'
 
  #H3K27 CHIP
 bsub -P baker -J 'H3K27-PeakCalling_CHIP' -N -n 8 -R "rusage[mem=16GB]" -R "span[hosts=1]" -q priority  \
