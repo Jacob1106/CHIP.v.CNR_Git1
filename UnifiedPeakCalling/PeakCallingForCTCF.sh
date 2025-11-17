e8cc008b21415d88988951d508c0373efc75f869
Jacob <jacob@martin-email.com>
2025-11-17 11:23:12 -0600

    Both CNR and CHIP
    added CHIP peak calling

--- UnifiedPeakCalling/PeakCallingForCTCF.sh
+++ UnifiedPeakCalling/PeakCallingForCTCF.sh
@@ -13,6 +13,18 @@ bsub -P baker -J 'H3K27-PeakCalling' -N -n 8 -R "rusage[mem=16GB]" -R "span[host
 --trackline \
 --outdir /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/CHIP.v.CNR_Git1/UnifiedPeakCalling'
 
+ #H3K27 CHIP
+bsub -P baker -J 'H3K27-PeakCalling_CHIP' -N -n 8 -R "rusage[mem=16GB]" -R "span[hosts=1]" -q priority  \
+'macs3 callpeak -t /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/ChIP.v.CnR/H3K27me3/ChIP/SJHGG059115_C129-LTC42_AAVS1_D7_R1-H3K27me3-AB4_REP1.mLb.clN.sorted.bam /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/ChIP.v.CnR/H3K27me3/ChIP/SJHGG059115_C130-LTC42_AAVS1_D7_R2-H3K27me3-AB4_REP1.mLb.clN.sorted.bam /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/ChIP.v.CnR/H3K27me3/ChIP/SJHGG059115_C131-LTC42_AAVS1_D7_R3-H3K27me3-AB4_REP1.mLb.clN.sorted.bam\
+-c /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/ChIP.v.CnR/H3K27me3/ChIP/SJHGG059115_C135-LTC42_AAVS1_D7-INPUT_REP1.mLb.clN.sorted.bam \
+-f BAM \
+-n H3KK27_PeakCalls_CHIP \
+-B \
+-p 0.0001 \
+--cutoff-analysis \
+--trackline \
+--outdir /research_jude/rgs01_jude/groups/bakergrp/projects/baker_data/bakergrp/analyses/JM/CHIP.v.CNR_Git1/UnifiedPeakCalling'
+
 
 
 
